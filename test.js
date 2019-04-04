5afcf6b8be47bbf651e096a3ec841cf677fa5fbe
GavIn Sokolof <sokolofg@gmail.com>
2019-04-03 23:46:14 -0400

    virtual working

--- test.js
+++ test.js
@@ -40,42 +40,42 @@ school.virtual("staffCount").get(function() {
 const School = mongoose.model("school", school);
 connect()
   .then(async connection => {
-    const schoolConfig = {
+    const schoolConfig = await School.create({
       name: "mlk elementry",
       openSince: 2009,
       students: 1000,
       isGreat: true,
       staff: ["e", "b", "g"]
-    };
-    const school2 = {
+    });
+    const school2 = await School.create({
       name: "Larry Middle School",
       openSince: 1980,
       students: 600,
       isGreat: false,
       staff: ["a", "b", "c"]
-    };
-    const schools = await School.create([schoolConfig, school2]);
+    });
+    // const schools = await School.create([schoolConfig, school2]);
     const match2 = await School.find({ staff: { $in: ["b", "g"] } })
       .sort("-openSince")
       .limit(1)
       .exec();
-    console.log(school2.staffCount, "yo", school2);
+    console.log(school2.staffCount);
   })
-  // const school = await School.findOneAndUpdate(
-  //   { name: "mlk elementry" },
-  //   { name: "mlk elementry" },
-  //   { upsert: true, new: true }
-  // ).exec();
-  // const student = await Student.create({
-  //   firstName: "Gavin",
-  //   school: school._id
-  // });
-  // const student2 = await Student.create({
-  //   firstName: "Luke",
-  //   school: school._id
-  // });
-  // const match = await Student.findById(student.id)
-  //   .populate()
-  //   .exec();
-
   .catch(e => console.error(e));
+
+// const school = await School.findOneAndUpdate(
+//   { name: "mlk elementry" },
+//   { name: "mlk elementry" },
+//   { upsert: true, new: true }
+// ).exec();
+// const student = await Student.create({
+//   firstName: "Gavin",
+//   school: school._id
+// });
+// const student2 = await Student.create({
+//   firstName: "Luke",
+//   school: school._id
+// });
+// const match = await Student.findById(student.id)
+//   .populate()
+//   .exec();
